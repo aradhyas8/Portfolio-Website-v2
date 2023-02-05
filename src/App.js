ba0cd118bd4a64a8dcfaa118f3c18759ae4626c9
Aradhya Singh <aradhyas1809@gmail.com>
2023-02-04 21:50:17 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
     <Experience />
     <Projects />
     <Contact />
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className="container">
+        <div className="footer"><p>Designed and Built by Aradhya.</p></div>
+    </div>
+    
+  )
+}
+
+export default Footer
